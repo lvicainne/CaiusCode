fe1cc8ebbd611e701a60580c00a2b22026209c9b
lvicainne <louis.vicainne@gmail.com>
2012-04-26 12:09:54 +0200

    Mise à jours des fichers binaires

--- gen/caius/code/R.java
+++ gen/caius/code/R.java
@@ -12,45 +12,69 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_menu_help=0x7f020001;
         public static final int ic_menu_info_details=0x7f020002;
-        public static final int ic_menu_invite=0x7f020001;
+        public static final int ic_menu_preferences=0x7f020003;
+        public static final int ic_menu_send=0x7f020004;
+        public static final int ic_menu_view=0x7f020005;
     }
     public static final class id {
-        public static final int about=0x7f060010;
-        public static final int addPerson=0x7f06000a;
-        public static final int addTask=0x7f06000b;
-        public static final int button1=0x7f060009;
-        public static final int button2=0x7f060008;
-        public static final int caiusMode=0x7f06000e;
-        public static final int editText1=0x7f060007;
-        public static final int item5=0x7f06000f;
-        public static final int menuMode=0x7f06000c;
-        public static final int morseMode=0x7f06000d;
-        public static final int radio1=0x7f060002;
-        public static final int radio2=0x7f060003;
-        public static final int radioGroup1=0x7f060001;
-        public static final int spinner1=0x7f060005;
-        public static final int textView1=0x7f060000;
-        public static final int textView3=0x7f060004;
-        public static final int textView4=0x7f060006;
+        public static final int about=0x7f07000f;
+        public static final int decodeRadio=0x7f070002;
+        public static final int encodeRadio=0x7f070001;
+        public static final int encodeTextView=0x7f070005;
+        public static final int help=0x7f070010;
+        public static final int input=0x7f070006;
+        public static final int itemOptions=0x7f07000e;
+        public static final int itemSend=0x7f07000a;
+        public static final int itemSendMail=0x7f07000c;
+        public static final int itemSendSms=0x7f07000b;
+        public static final int itemShow=0x7f07000d;
+        public static final int leftButton=0x7f070008;
+        public static final int output=0x7f070007;
+        public static final int radioGroup1=0x7f070000;
+        public static final int rightButton=0x7f070009;
+        public static final int spinner1=0x7f070004;
+        public static final int textView3=0x7f070003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class menu {
-        public static final int menu=0x7f050000;
+        public static final int menu=0x7f060000;
     }
     public static final class string {
-        public static final int about=0x7f040003;
-        public static final int addPerson=0x7f040001;
-        public static final int addTask=0x7f040002;
-        public static final int app_name=0x7f040000;
-        public static final int caius=0x7f040008;
-        public static final int delete=0x7f040006;
-        public static final int edit=0x7f040005;
-        public static final int menuMode=0x7f040009;
-        public static final int menuModeShort=0x7f04000a;
-        public static final int morse=0x7f040007;
-        public static final int name=0x7f040004;
+        public static final int about=0x7f050002;
+        public static final int aboutContent=0x7f050014;
+        public static final int app_name=0x7f050000;
+        public static final int binaryCode=0x7f050015;
+        public static final int caius=0x7f050007;
+        public static final int clear=0x7f05000d;
+        public static final int codeType=0x7f050010;
+        public static final int decode=0x7f05000b;
+        public static final int decodeText=0x7f05000f;
+        public static final int delete=0x7f050005;
+        public static final int edit=0x7f050004;
+        public static final int emptyString=0x7f050012;
+        public static final int encode=0x7f05000a;
+        public static final int encodeText=0x7f05000e;
+        public static final int help=0x7f050001;
+        public static final int helpContent=0x7f05001b;
+        public static final int menuMode=0x7f050008;
+        public static final int menuModeShort=0x7f050009;
+        public static final int morse=0x7f050006;
+        public static final int morseCode=0x7f050011;
+        public static final int name=0x7f050003;
+        public static final int ok=0x7f050013;
+        public static final int options=0x7f050016;
+        public static final int prefOutput=0x7f05001c;
+        public static final int preferencesSaved=0x7f050018;
+        public static final int send=0x7f05000c;
+        public static final int sendMail=0x7f05001a;
+        public static final int sendSms=0x7f050019;
+        public static final int show=0x7f050017;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
